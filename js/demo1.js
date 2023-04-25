aea374a1ed041a4c648351968455ea6ca6c988e5
MDavid Low <mdavidlow@gmail.com>
2023-04-25 15:14:36 -0700

    Update demo1.js

--- js/demo1.js
+++ js/demo1.js
@@ -3,9 +3,9 @@ var width = canvas.offsetWidth,
     height = canvas.offsetHeight;
 
 var colors = [
-    new THREE.Color(0xac1122),
-    new THREE.Color(0x96789f),
-    new THREE.Color(0x535353)];
+    new THREE.Color(0x00AFC4),
+    new THREE.Color(0x00FFEC),
+    new THREE.Color(0xCEFF00)];
 
 var renderer = new THREE.WebGLRenderer({
     canvas: canvas,
